563164f9a79ab169616d4dcbc587997672a07d22
Pranshu Malhotra <pranshudee8@gmail.com>
2021-08-10 22:57:59 +0530

    vga buffer interface for printing to the screen

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
-// called on panic.
+/// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
@@ -17,14 +19,22 @@ static HELLO: &[u8] = b"Hello World!";
 **/
 #[no_mangle] // disable name mangling
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
 
+    // vga_buffer::print_something();
+
+    // use core::fmt::Write;
+    // vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+
+    println!("Hello World {} {}", "!", "abc");
+    panic!("Some panic message");
     loop {}
 }
